a2134ba692b75a6b3a24f5f45198ef3661163155
´M4rkus117´ <marknav17@gmail.com>
2026-05-01 23:25:53 -0600

    Revert "Add files via upload"
    This reverts commit e5fa4592c7a4b83ea87ac9a6ed95f1fd37af2a9e.

--- tareasPOO/tarea10/pruebasT10.java
+++
@@ -1,9 +0,0 @@
-package tareasPOO.tarea10;
-
-public class pruebasT10 {
-    public static void main(){
-        //Ventana ventana = new Ventana("Ejemplo Ventana", 720, 600);
-        //VentanaBorderLayout ventana2=new VentanaBorderLayout();
-        VentanaGridLayout ventana3=new VentanaGridLayout();
-    }
-}
--- tareasPOO/tarea7/Empleado.java
+++
@@ -1,8 +0,0 @@
-package tareasPOO.tarea7;
-
-public interface Empleado {
-    public static final int EMPLEADO_TIEMPO_COMPLETO = 1;
-    public static final int EMPLEADO_POR_HORAS = 2;
-
-    public float calcularSalario();
-}
